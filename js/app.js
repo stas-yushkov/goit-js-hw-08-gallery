b2e0f0203f2ed891e8d78843761df313e1bce900
stas-yushkov <stas.yushkov@gmail.com>
2021-06-12 00:18:44 +0300

    upd

--- js/app.js
+++ js/app.js
@@ -63,3 +63,24 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+// Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+// Открытие модального окна по клику на элементе галереи.
+// Подмена значения атрибута src элемента img.lightbox__image.
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+// Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+
+// <li class="gallery__item">
+//   <a
+//     class="gallery__link"
+//     href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//   >
+//     <img
+//       class="gallery__image"
+//       src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+//       data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//       alt="Tulips"
+//     />
+//   </a>
+// </li>;
