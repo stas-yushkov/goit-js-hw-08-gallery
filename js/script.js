4c786249767364f22ee164a2f3601a2ef61bc868
stas-yushkov <stas.yushkov@gmail.com>
2021-06-13 22:01:32 +0300

    some ref upd

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import galleryItems from '../js/app.js';
+import galleryItems from './js/app.js';
 
 const galleryRef = document.querySelector('.js-gallery');
 const lightboxRef = document.querySelector('.js-lightbox');
